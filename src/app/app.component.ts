d80b63f3b901abb4fec4692d1df6961fcb0df322
jaffreg <103439297+jaffreg@users.noreply.github.com>
2022-10-05 12:11:47 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
-import { MenuItem } from 'primeng/api';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   texte: string =
-    'ABCD\nDDD\n1234ABCDF\nEEE\n123456\nAZRRRFD\n12365\n344R4\nPOIUY' +
-    'ABCD\nDDD\n1234ABCDF\nEEE\n123456\nAZRRRFD\n12365\n344R4\nPOIUY';
+    'ABCD\nDDD\n1234ABCDF\nEEE\n123456' + 'AZRRRFD\n12365\n344R4\nPOIUY';
+
+  public form: FormControl = this._formBuilder.control(
+    this.texte,
+    Validators.maxLength(255)
+  );
+  public formGroup: FormGroup = this._formBuilder.group({
+    texte: this.form,
+  });
+
+  constructor(private _formBuilder: FormBuilder) {}
+
+  init() {
+    //this.formGroup.get('texte').setValue(this.texte);
+  }
+
+  ngOnInit(): void {
+    setTimeout(() => this.init(), 1000);
+  }
+
+  modifier() {
+    this.texte = this.texte + this.texte;
+    //this.formGroup.get('texte').setValue(this.texte);
+    this.formGroup.patchValue({ texte: this.texte });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppComponent }   from './app.component';
+import { AppComponent } from './app.component';
 
-import {InputTextareaModule} from 'primeng/inputtextarea';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { PanelModule } from 'primeng/panel';
 
 @NgModule({
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     InputTextareaModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    PanelModule,
   ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
 })
-
-export class AppModule { }
+export class AppModule {}
