a8ec3f43faa1629288f362f81e8d91e0a9fc6da9
jaffreg <103439297+jaffreg@users.noreply.github.com>
2022-10-05 10:43:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# primeng-inputtextarea-demo-jtwcsr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/primeng-inputtextarea-demo-qijo7n)
---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  texte: string =
+    'ABCD\nDDD\n1234ABCDF\nEEE\n123456\nAZRRRFD\n12365\n344R4\nPOIUY' +
+    'ABCD\nDDD\n1234ABCDF\nEEE\n123456\nAZRRRFD\n12365\n344R4\nPOIUY';
+}
